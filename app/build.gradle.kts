a7d379f7a0f72df4094185db58cf6be2dc4bbf4a
Nibha Maharjan <nibhamaharjan@yahoo.com>
2024-03-19 22:47:18 -0400

    3 Animations done 1 Left

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,9 @@ android {
         }
     }
 }
-
 dependencies {
+
+    // Existing dependencies
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
@@ -59,6 +60,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,4 +70,9 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Add the Compose Animation library
+    implementation("androidx.compose.animation:animation-core:1.0.0")
+    implementation ("androidx.navigation:navigation-compose:2.4.0-alpha09")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.1.0")
 }
--- app/src/main/java/com/example/mapd721_a3_nibhamaharjan/MainActivity.kt
+++ app/src/main/java/com/example/mapd721_a3_nibhamaharjan/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.mapd721_a3_nibhamaharjan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.mapd721_a3_nibhamaharjan.ui.theme.MAPD721A3NibhaMaharjanTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +27,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MAPD721A3NibhaMaharjanTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Nibha")
+                    val navController = rememberNavController()
+                    Navigation(navController)
                 }
             }
         }
@@ -30,17 +40,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Navigation(navController: NavHostController) {
+    NavHost(navController, startDestination = "home") {
+        composable("home") { HomePage(navController) }
+        composable("transitionAnimation") { TransitionAnimationPage() }
+        composable("scaleAnimation") { ScaleAnimationPage() }
+        composable("infiniteAnimation") { InfiniteAnimationPage() }
+        composable("enterExitAnimationPage") { EnterExitAnimationPage() }
+    }
+}
+
+@Composable
+fun HomePage(navController: NavHostController) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                HomeButton(text = "Transition Animation") {
+                    navController.navigate("transitionAnimation")
+                }
+                HomeButton(text = "Scale Animation", onClick = {
+                    navController.navigate("scaleAnimation")
+                })
+                HomeButton(text = "Infinite Animation", onClick = {
+                    navController.navigate("infiniteAnimation")
+                })
+                HomeButton(text = "Enter Exit Animation", onClick = {
+                    navController.navigate("enterExitAnimationPage")
+                })
+            }
+        }
+    }
+}
+
+
+@Composable
+fun HomeButton(text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Text(text = text)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomePagePreview() {
     MAPD721A3NibhaMaharjanTheme {
-        Greeting("Android")
+        HomePage(navController = rememberNavController())
     }
 }
